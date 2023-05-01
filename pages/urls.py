978779ee4238d09dc921aa65ea26ed8acdfbc80d
OumaSylvester <reubensleeq@gmail.com>
2023-05-01 12:47:53 +0300

    Posts, View lists from db

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
-    path("", HomePageView.as_view(), name="home"),
+    path("home/", HomePageView.as_view(), name="home"),
 ]
 
 
